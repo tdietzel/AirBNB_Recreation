befd421e0292b0298c0493210819598b24fcc371
tdietzel22 <147360049+tdietzel22@users.noreply.github.com>
2023-12-04 14:10:05 -0800

    Initial Commit

---
+++ AirBNBRecreation/src/components/Header.jsx
@@ -0,0 +1,17 @@
+function Header() {
+  return (
+    <>
+      <nav className="navbar bg-body-tertiary">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            <img src="" alt="Logo" width="30" height="24" className="d-inline-block align-text-top"/>
+            AirBnB
+          </a>
+        </div>
+      </nav>
+      <button className="btn btn-dark">Test</button>
+    </>
+  )
+}
+
+export default Header;
--- AirBNBRecreation/src/main.jsx
+++ AirBNBRecreation/src/main.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import Header from '../src/components/Header.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <>
+    <Header />
+    {/* <Filters />
+    <Results />
+    <ContinueExploring />
+    <Suggestions />
+    <Footer />  */}
+  </>
 )
