f0bd0a870afb3dd722714731e2462dbc8441a71e
tdietzel22 <147360049+tdietzel22@users.noreply.github.com>
2023-12-04 16:29:59 -0800

    Update header and styling for it
    Co-authored-by: Teddy Peterschmidt <Teddyp14@users.noreply.github.com>

--- AirBNBRecreation/src/components/Header.jsx
+++ AirBNBRecreation/src/components/Header.jsx
@@ -1,17 +1,55 @@
+import AirbnbLogo from "./../img/Airbnb-logo.jpg";
+
 function Header() {
   return (
     <>
       <nav className="navbar bg-body-tertiary">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">
-            <img src="" alt="Logo" width="30" height="24" className="d-inline-block align-text-top"/>
-            AirBnB
+          {/* Air BNB Logo */}
+          <a className="navbar-brand" href="#" style={{ marginLeft: '69px' }}>
+          <img src={AirbnbLogo} width="98" height="53"></img>
           </a>
+          {/* Search Destinations */}
+          <div className="btn-group" role="group" id="searchButton" aria-label="Basic outlined example">
+              <button type="button" className="btn btn-outline-primary">Anywhere | Any week | Add guests
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                  <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
+                </svg> 
+              </button>
+          </div>
+          <div id="headerRight" className="d-flex justify-content-end align-items-center">
+            {/* List Your Home */}
+            <button type="button" className="btn btn-outline-primary" id="listHome">Airbnb your home</button>
+
+            {/* Region */}
+            <button className="btn btn-outline-primary" id="region">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-globe" viewBox="0 0 16 16">
+                <path d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m7.5-6.923c-.67.204-1.335.82-1.887 1.855A7.97 7.97 0 0 0 5.145 4H7.5zM4.09 4a9.267 9.267 0 0 1 .64-1.539 6.7 6.7 0 0 1 .597-.933A7.025 7.025 0 0 0 2.255 4zm-.582 3.5c.03-.877.138-1.718.312-2.5H1.674a6.958 6.958 0 0 0-.656 2.5zM4.847 5a12.5 12.5 0 0 0-.338 2.5H7.5V5zM8.5 5v2.5h2.99a12.495 12.495 0 0 0-.337-2.5zM4.51 8.5a12.5 12.5 0 0 0 .337 2.5H7.5V8.5zm3.99 0V11h2.653c.187-.765.306-1.608.338-2.5zM5.145 12c.138.386.295.744.468 1.068.552 1.035 1.218 1.65 1.887 1.855V12zm.182 2.472a6.696 6.696 0 0 1-.597-.933A9.268 9.268 0 0 1 4.09 12H2.255a7.024 7.024 0 0 0 3.072 2.472M3.82 11a13.652 13.652 0 0 1-.312-2.5h-2.49c.062.89.291 1.733.656 2.5zm6.853 3.472A7.024 7.024 0 0 0 13.745 12H11.91a9.27 9.27 0 0 1-.64 1.539 6.688 6.688 0 0 1-.597.933M8.5 12v2.923c.67-.204 1.335-.82 1.887-1.855.173-.324.33-.682.468-1.068zm3.68-1h2.146c.365-.767.594-1.61.656-2.5h-2.49a13.65 13.65 0 0 1-.312 2.5zm2.802-3.5a6.959 6.959 0 0 0-.656-2.5H12.18c.174.782.282 1.623.312 2.5zM11.27 2.461c.247.464.462.98.64 1.539h1.835a7.024 7.024 0 0 0-3.072-2.472c.218.284.418.598.597.933zM10.855 4a7.966 7.966 0 0 0-.468-1.068C9.835 1.897 9.17 1.282 8.5 1.077V4z"/>
+              </svg>
+            </button>
+
+            {/* Profile DropDown Icon*/}
+            <div className="dropdown" id="dropdown-button" style={{ marginRight: '72px' }}>
+              <button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-person-circle" viewBox="0 0 16 16">
+                  <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
+                  <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
+                </svg>
+              </button>
+              {/* Profile DropDown */}
+              <ul className="dropdown-menu dropdown-menu-lg-end">
+                <li><a className="dropdown-item" href="#">Winter Release features</a></li>
+                <li><a className="dropdown-item" href="#">Sign up</a></li>
+                <li><a className="dropdown-item" href="#">Log in</a></li>
+                <li><a className="dropdown-item" href="#">Gift cards</a></li>
+                <li><a className="dropdown-item" href="#">AirBnB your home</a></li>
+                <li><a className="dropdown-item" href="#">Help Center</a></li>
+              </ul>
+          </div>
+        </div>
         </div>
       </nav>
-      <button className="btn btn-dark">Test</button>
     </>
   )
 }
-
 export default Header;
